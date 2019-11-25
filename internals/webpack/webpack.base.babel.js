9116c3dfbc71e651444a10505d5222f57aa7f17e
Harry Pua <harrypua@gmail.com>
2019-11-25 13:10:31 +0800

    removed gifsicle and ngrok

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -69,27 +69,30 @@ module.exports = options => ({
             },
           },
           {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                enabled: false,
-                // NOTE: mozjpeg is disabled as it causes errors in some Linux environments
-                // Try enabling it in your environment by switching the config to:
-                // enabled: true,
-                // progressive: true,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              optipng: {
-                optimizationLevel: 7,
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4,
-              },
-            },
+            loader: 'file-loader',
           },
+          // {
+          //   loader: 'image-webpack-loader',
+          //   options: {
+          //     mozjpeg: {
+          //       enabled: false,
+          //       // NOTE: mozjpeg is disabled as it causes errors in some Linux environments
+          //       // Try enabling it in your environment by switching the config to:
+          //       // enabled: true,
+          //       // progressive: true,
+          //     },
+          //     gifsicle: {
+          //       interlaced: false,
+          //     },
+          //     optipng: {
+          //       optimizationLevel: 7,
+          //     },
+          //     pngquant: {
+          //       quality: '65-90',
+          //       speed: 4,
+          //     },
+          //   },
+          // },
         ],
       },
       {
